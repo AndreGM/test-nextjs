d1b143a14627ab4a42a70eaec3bb548cca56d8f3
andregm <andre@appdev.com.br>
2025-11-20 11:28:59 -0300

    chore: initial configuration next, tailwind, shadcn, vitest, react testing library, tanstack query, zod.

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { TanstackProvider } from "@/components/providers/tanstack-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Teste Dev Frontend - Next.js",
+  description: "Teste Dev Frontend utilizando Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <TanstackProvider>{children}</TanstackProvider>
+      </body>
+    </html>
+  );
+}
